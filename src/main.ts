8ab03b4de7ed66509062ec9f2c7c7a4088e4953d
Arif Rahman Hakim <new.arifrh199@gmail.com>
2024-07-02 13:06:58 +0800

    style: setp theme

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,20 @@
-import "./assets/main.css";
-
+import "./styles/base.css";
 import { createApp, markRaw } from "vue";
 import { createPinia } from "pinia";
-import 'element-plus/theme-chalk/el-notification.css';
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-
+import "element-plus/theme-chalk/el-notification.css";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import App from "./App.vue";
 import router from "./router";
+import "./styles/theme.css";
 
 const app = createApp(App);
 const pinia = createPinia();
 
 pinia.use(({ store }) => {
-    store.router = markRaw(router);
+  store.router = markRaw(router);
 });
 
-pinia.use(piniaPluginPersistedstate)
+pinia.use(piniaPluginPersistedstate);
 
 app.use(pinia);
 app.use(router);
