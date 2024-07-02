f06dbf18d6b4e839f3454534b5597e662119b759
Arif Rahman Hakim <new.arifrh199@gmail.com>
2024-07-02 14:28:16 +0800

    chore: disable button login with sso

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,69 +1,39 @@
 <template>
   <div class="container">
-    <el-card
-      style="
+    <el-card style="
         width: 480px;
         padding-left: 20px;
         padding-right: 20px;
         padding-top: 10px;
         padding-bottom: 5px;
         border-radius: 30px;
-      "
-    >
-      <el-text style="font-size: 22pt; font-weight: 500"
-        >Selamat Datang! 👋</el-text
-      >
-      <el-form
-        @submit.prevent
-        style="margin-top: 15px"
-        :rules="rules"
-        ref="formRef"
-        v-loading="loading"
-        :model="form"
-        label-width="auto"
-        label-position="top"
-      >
+      ">
+      <el-text style="font-size: 22pt; font-weight: 500">Selamat Datang! 👋</el-text>
+      <el-form @submit.prevent style="margin-top: 15px" :rules="rules" ref="formRef" v-loading="loading" :model="form"
+        label-width="auto" label-position="top">
         <el-form-item label="Email" required prop="email">
-          <el-input
-            @keyup.enter="login(formRef)"
-            size="large"
-            v-model="form.email"
-            type="email"
-            placeholder="Masukkan Email"
-          />
+          <el-input @keyup.enter="login(formRef)" size="large" v-model="form.email" type="email"
+            placeholder="Masukkan Email" />
         </el-form-item>
         <el-form-item label="Password" required prop="password">
-          <el-input
-            @keyup.enter="login(formRef)"
-            size="large"
-            v-model="form.password"
-            type="password"
-            placeholder="Masukkan Password"
-          />
+          <el-input @keyup.enter="login(formRef)" size="large" v-model="form.password" type="password"
+            placeholder="Masukkan Password" />
         </el-form-item>
         <el-form-item style="margin-top: 30px">
-          <el-button
-            type="primary"
-            size="large"
-            style="width: 100%"
-            @click="login(formRef)"
-            >Login</el-button
-          >
+          <el-button type="primary" size="large" style="width: 100%" @click="login(formRef)">Login</el-button>
         </el-form-item>
       </el-form>
-      <div style="display: flex; align-items: center">
-        <el-divider style="margin-right: 10px"></el-divider>
-        <span style="margin: 0 10px">OR</span>
-        <el-divider style="margin-left: 10px"></el-divider>
+      <div v-if="enableSso">
+        <div style="display: flex; align-items: center">
+          <el-divider style="margin-right: 10px"></el-divider>
+          <span style="margin: 0 10px">OR</span>
+          <el-divider style="margin-left: 10px"></el-divider>
+        </div>
+        <el-button size="large" style="margin-top: 18px; width: 100%" @click="login(formRef)">
+          <IconBPS style="width: 1.9em; height: 1.9em; margin-right: 10px" />
+          Login with SSO BPS
+        </el-button>
       </div>
-
-      <el-button
-        size="large"
-        style="margin-top: 18px; width: 100%"
-        @click="login(formRef)"
-        ><IconBPS style="width: 1.9em; height: 1.9em; margin-right: 10px" />
-        Login with SSO BPS</el-button
-      >
     </el-card>
   </div>
 </template>
@@ -78,6 +48,8 @@ const initialState = {
   email: "",
   password: "",
 };
+
+const enableSso = ref(false);
 
 const rules = reactive<FormRules<any>>({
   email: [
@@ -162,9 +134,8 @@ watch(
   justify-content: center;
   align-items: center;
   height: 100vh;
-  background-color: var(
-    --el-color-primary-light-5
-  ); /* Example background color */
+  background-color: var(--el-color-primary-light-5);
+  /* Example background color */
 }
 
 .card {
