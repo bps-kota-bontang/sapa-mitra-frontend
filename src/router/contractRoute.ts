b154940652fd8054f5c9bcedbc8a8632fd795d72
Arif Rahman Hakim <new.arifrh199@gmail.com>
2024-07-08 08:19:14 +0800

    chore: tu lead can create contract

--- src/router/contractRoute.ts
+++ src/router/contractRoute.ts
@@ -1,39 +1,47 @@
-import ListContractView from '@/views/contract/ListContractView.vue';
-import CreateContractView from '@/views/contract/CreateContractView.vue';
-import { useUserStore } from '@/stores/user';
-import type { NavigationGuardNext, RouteLocationNormalized } from 'vue-router';
+import ListContractView from "@/views/contract/ListContractView.vue";
+import CreateContractView from "@/views/contract/CreateContractView.vue";
+import { useUserStore } from "@/stores/user";
+import type { NavigationGuardNext, RouteLocationNormalized } from "vue-router";
 
 const contractRoutes = [
-    {
-        path: '/spk',
-        meta: {
-            layout: 'LayoutDashboard',
-            requiresAuth: true
-        },
-        children: [
-            {
-                path: '',
-                name: 'listContract',
-                component: ListContractView,
-                meta: {
-                    title: "Daftar SPK"
-                }
-            },
-            {
-                path: 'buat',
-                name: 'createContract',
-                component: CreateContractView,
-                meta: {
-                    title: "Buat SPK"
-                },
-                beforeEnter: (to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) => {
-                    const user = useUserStore();
-                    if (!["ANGGOTA"].includes(user.position)) next({ "name": "unauthorized" })
-                    else next();
-                },
-            }
-        ]
+  {
+    path: "/spk",
+    meta: {
+      layout: "LayoutDashboard",
+      requiresAuth: true,
     },
+    children: [
+      {
+        path: "",
+        name: "listContract",
+        component: ListContractView,
+        meta: {
+          title: "Daftar SPK",
+        },
+      },
+      {
+        path: "buat",
+        name: "createContract",
+        component: CreateContractView,
+        meta: {
+          title: "Buat SPK",
+        },
+        beforeEnter: (
+          to: RouteLocationNormalized,
+          from: RouteLocationNormalized,
+          next: NavigationGuardNext
+        ) => {
+          const user = useUserStore();
+          if (
+            !["ANGGOTA"].includes(user.position) &&
+            !["TU"].includes(user.team)
+          )
+            next({ name: "unauthorized" });
+          else next();
+        },
+      },
+    ],
+  },
 ];
 
 export default contractRoutes;
