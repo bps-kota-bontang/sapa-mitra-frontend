4da9ed5064dc0164ef4631dc81c9992050bcdf6a
Arif Rahman Hakim <new.arifrh199@gmail.com>
2024-07-11 07:05:04 +0800

    chore: add build hash in footer

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
-    <el-text
-      >© 2024. Made with ❤️ by BPS Kota Bontang. (Versi: 1.0.0
-      {{ mode }})</el-text
-    >
+    <el-text>© 2024. Made with ❤️ by BPS Kota Bontang. (Versi: 1.0.0
+      {{ mode }}, Build: {{ build }})</el-text>
   </div>
 </template>
 
 <script setup>
 const mode = import.meta.env.MODE;
+const build = import.meta.env.VITE_BUILD_HASH;
 </script>
